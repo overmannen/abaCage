7f297490e5cd2765896639243f1d8dd8c9453019
overmannen <jesper231@gmail.com>
2025-09-28 22:01:20 +0200

    Fix deployment for api and frontend

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from logic import create_round
+import os
 
 
 class CreateRoundRequest(BaseModel):
@@ -13,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://abacage-7349b.web.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -59,4 +60,5 @@ def create_round_request(request: CreateRoundRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
