32b829941c2a72b6b176871796f0d78bd2ec6fe4
overmannen <jesper231@gmail.com>
2025-10-19 10:25:01 +0200

    fix_path

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const isDevolpment = import.meta.env.DEV;
 
 export const API_BASE_URL = isDevolpment
-  ? "http://jespertl.folk.ntnu.no:8000"
-  : "http://jespertl.folk.ntnu.no:8000/";
+  ? "http://localhost:8000"
+  : "https://abacage.onrender.com";
